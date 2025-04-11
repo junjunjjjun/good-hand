4a74812c5a36e2019c7b4e1ccd8381bcd7723a54
junjunjjjun <junoo.par@gmail.com>
2025-04-11 17:01:48 +0800

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				caveat: ["Caveat Brush", "serif"],
+				caveat: ["Times New Roman", "serif"],
 			},
 			container: {
 				center: true,
